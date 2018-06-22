68033e97894dd321592024349f4d5dfbef48d41b
Emir Mehic <emir92mm@gmail.com>
2018-06-22 22:36:24 +0200

    Added minification

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from 'rollup-plugin-uglify';
 
 export default {
   input: 'src/index.js',
@@ -11,7 +12,8 @@ export default {
     commonjs(),
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    uglify()
   ],
   external: ['uuid']
 };
